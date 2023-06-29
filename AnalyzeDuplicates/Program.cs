57f15a620633a43f14fbf36ba35c45b1980b4301
zetabeb <zambenber@hotmail.com>
2023-06-28 23:15:13 -0500

    find duplicates

--- AnalyzeDuplicates/Program.cs
+++ AnalyzeDuplicates/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ZBB;
+using System.IO;
+namespace AnalizeDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string root = @"D:\ZAMIR\libros-20230626T163654Z-001\libros";
+            //List<string> listFolders = new List<string>();
+            //listFolders = ZBB.FileManager.ListarDirectorios(ruta);
+            //listFolders.Add(ruta);
+            string[] FoldersList = Directory.GetDirectories(root, "*", SearchOption.AllDirectories);
+            string[] FilesList = Directory.GetFiles(root, "*", SearchOption.AllDirectories);
+            string[] FilesListCompare = Directory.GetFiles(root, "*", SearchOption.AllDirectories);
+            int folderCount = FoldersList.Count();
+            int fileCount = FilesList.Count();
+            
+            List<DuplicateFiles> DuplicateFilesList = new List<DuplicateFiles>();
+
+            Console.WriteLine("Folders: \n");
+            foreach (string folder in FoldersList)
+            {
+                Console.WriteLine(folder);
+            }
+
+            Console.WriteLine("\n\nFiles:\n");
+            foreach (string files in FilesList)
+            {
+                Console.WriteLine(files);
+            }
+                        
+            foreach (string file in FilesList)
+            {                
+                DuplicateFiles duplicateFiles = new DuplicateFiles();
+                List<string> files = new List<string>();
+                
+                FileInfo file1 = new FileInfo(file);
+                string fileName = file1.Name;
+
+                files.Add(file);
+                duplicateFiles.Name = fileName;                
+
+                for (int i=0; i < FilesListCompare.Length; i++)
+                {
+                    if (FilesListCompare[i] == "") continue;
+                    FileInfo file2 = new FileInfo(FilesListCompare[i]);
+                    string fileCompareName = file2.Name;                    
+                    
+                    if(fileName == fileCompareName)
+                    {                        
+                        if (file != FilesListCompare[i])
+                        {
+                            files.Add(FilesListCompare[i]);                            
+                        }
+                        FilesListCompare[i] = "";
+                    }                    
+                }
+                if (files.Count > 1)
+                {
+                    duplicateFiles.Files.AddRange(new List<string>(files));
+                    DuplicateFilesList.Add(duplicateFiles);
+                }
+                
+                files.Clear();
+            }
+
+            Console.WriteLine("\n\nDuplicados:\n");
+            foreach(var d in DuplicateFilesList)
+            {
+                Console.WriteLine(d.Name);
+                foreach(var df in d.Files)
+                {
+                    Console.WriteLine(df);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    public class DuplicateFiles
+    {
+        public string Name { get; set; }
+        public List<string> Files { get; set; } = new List<string>();
+    }
+}
--- AnalyzeDuplicates/ZBB/FileManager.cs
+++ AnalyzeDuplicates/ZBB/FileManager.cs
@@ -52,5 +52,49 @@ namespace ZBB
                 Console.WriteLine("Error al borrar archivo origen: {0}", e.ToString());
             }
         }
+        public static List<string> ListarDirectorios(string? dir)
+        {
+            List<string> levelOneList = new List<string>();
+            List<string> list = new List<string>();
+            string[] dirs = Directory.GetDirectories(dir);
+
+            foreach (string subDir in dirs)
+            {
+                levelOneList.Add(subDir);                
+            }
+
+            List<string> result = new List<string>();
+            result = levelOneList;
+            list.AddRange(result);
+            do
+            {
+                foreach (var item in result)
+                {
+                    result = GetSubDirectorio(item);
+                    list.AddRange(result);
+                }
+            } 
+            while (result.Count > 0);
+
+            return list;
+        }
+        private static List<string> GetSubDirectorio(string? dir)
+        {
+            string[] dirs = Directory.GetDirectories(dir);
+            List<string> subDirs = new List<string>();
+            //foreach (string subDir in dirs)
+            //{
+            //    subDirs.Add(subDir);
+            //}
+            
+                foreach (string subDir in dirs)
+                {
+                    subDirs.Add(subDir);
+                    GetSubDirectorio(subDir);
+                }
+            
+            
+            return subDirs;
+        }
     }
 }
